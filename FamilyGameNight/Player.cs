af17d0a210a42a22f7c2ecb428777e2b0928ec6e
knunn553 <knunn553@outlook.com>
2024-08-09 12:01:32 -0400

    Made video on FamilyGameNight project. Left of trying to build to LINQ and then then create a for loop in the method that displays everything. I began stepping through the debugger. I want to try to begin seeing what is happening in the code rather than relying on ChatGPT for the answer. So, we will continue down that route next time when we continue with the FamilyGameNight project.

---
+++ FamilyGameNight/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FamilyGameNight
+{
+    
+    public class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PreviousScore { get; set; }
+        public int RecentScore { get; set; }
+
+        public Player(string firstname, string lastname, int previousscore, int recentscore)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.PreviousScore = previousscore;
+            this.RecentScore = recentscore;
+        }
+
+        public static void filterPlayers(List<Player> players, int scoreDifferential)
+        {
+            var filteredAndGroupedPlayers = players
+                .Where(p => (p.RecentScore - p.PreviousScore) > scoreDifferential)
+                .GroupBy(p => p.LastName.First())
+                .OrderBy(p => p.Key);
+
+            foreach (Group filteredPlayers in filteredAndGroupedPlayers)
+            {
+                Console.WriteLine($"Those with first initial {filteredPlayers.Value} of last name starting with");
+            }
+        }
+
+
+    }
+}
---
+++ FamilyGameNight/Program.cs
@@ -0,0 +1,18 @@
+﻿using FamilyGameNight;
+using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+
+List<Player> playerList = new List<Player>();
+
+playerList.Add(new Player("Kimbo", "Slice", 233, 211));
+playerList.Add(new Player("Siddhartha", "Guatama", 200, 250));
+playerList.Add(new Player("George", "Washington", 144, 132));
+playerList.Add(new Player("Derek", "Jeter", 200, 178));
+playerList.Add(new Player("Dua", "Lipa", 200, 300));
+
+Player.filterPlayers(playerList, 10);
+
+
+
+
