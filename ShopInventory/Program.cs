2e76568130f00b221f0ea404ef849976a09ce6c0
knunn553 <knunn553@outlook.com>
2024-08-08 20:22:28 -0400

    For the indexer item, we struggled with this, especially trying to implement try catch and throwing exceptions in the set and get accessors, which isn't really where we should be putting the logic anyways. Learned about indexers and got to practice everything else. Need to learn more about how to handle exceptionps, though.

---
+++ ShopInventory/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Security;
+
+namespace ShopInventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           
+            var grocery = new Shop();
+            Console.WriteLine("Welcome to John's Grocery!");
+            Console.WriteLine("Press 1 to Enter a New Item.");
+            Console.WriteLine("Press 2 to Exit.");
+            var userInput = Console.ReadLine();
+            
+            int dictionaryIndexKey = 0;
+
+
+            while (userInput != "2") 
+            {
+                 // Question: Why do we have to move this above the while loop? 
+                // Question: How would we have figured out the problem with this in the debugger?
+                string dictionaryStringValue = "";
+                // Best way to think of a switch case statement. An alternative is an if else, but we use switch case if we have a very small amount of options.
+                // we are switching the logic depending on the case.
+                // To the switch statement will contain a variable that has the ability to be changed
+                // The case statements give all the possible, well, cases or possibilities for the particular variable.
+
+                if (userInput == "1") 
+                {
+                    // How do I know if I've reached to point in my code where I have created an infinite loop?
+                    // Check CPU
+                    // Check Call Stack
+                    Console.Write("Item Name: ");
+                    dictionaryStringValue = Console.ReadLine();
+                    grocery[dictionaryIndexKey] = dictionaryStringValue;
+                    if(grocery.DuplicateItems(dictionaryStringValue) == false)
+                    {
+                        Console.WriteLine("Item Added!");
+                    }
+                    dictionaryIndexKey++;
+                }
+
+                
+
+                Console.WriteLine("Press 1 to Enter a New Item.");
+                Console.WriteLine("Press 2 to Exit.");
+                userInput = Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
---
+++ ShopInventory/Shop.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopInventory
+{
+
+    internal class Shop
+    {
+        
+        Dictionary<int, string> items;
+
+        public Shop() 
+        {
+            items = new Dictionary<int, string>();
+        }
+
+
+        public string this[int index]
+        {
+            get
+            {
+                if (items.ContainsKey(index))
+                {
+                    return items[index];
+                }
+                else
+                {
+                    throw new KeyNotFoundException("This item has not been added or assigned yet. Please add it to the dictionary.");
+                }
+            }
+            set
+            {
+                try
+                {
+                    // Check if the key already exists
+                    if (items.ContainsKey(index))
+                    {
+                        throw new ArgumentException("An item with the same key already exists.");
+                    }
+
+                    // Check if the value already exists
+                    if (DuplicateItems(value))
+                    {
+                        throw new ArgumentException("This item has already been inserted.");
+                    }
+
+                    // Add the new key-value pair to the dictionary
+                    items[index] = value;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    // Optionally rethrow the exception or handle it as needed
+                    // throw;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Unexpected error: {ex.Message}");
+                    // Optionally rethrow the exception or handle it as needed
+                    // throw;
+                }
+
+            }
+        }
+
+        public IEnumerable<KeyValuePair<int, string>> GetAllPairs()
+        {
+            return items;
+        }
+
+        public bool DuplicateItems(string value)
+        {
+            foreach(var kvp in items.Values)
+            {
+                if(kvp.Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
