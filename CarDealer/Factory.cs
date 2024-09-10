2da47f95d0c980b7b1f077f246357869db29d168
knunn553 <knunn553@outlook.com>
2024-09-10 09:49:00 -0400

    Adding final LinkedIn Learning challenge, which focused on Reflection in C# .NET. Learned that Reflection is good for understanding an applications metadata, or the data about the data. In other words, Reflection allows us to do things like use ORM like Entity Framework, Serialize JSON into Objects, and even work with Unit Testing frameworks like XUnit, MTest, and Others.

---
+++ CarDealer/Factory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer
+{
+    public class Factory
+    {
+        public abstract class Car() { };
+
+        public class Jeep : Car { };
+        public class Ram : Car { };
+        public class Suburu : Car { };
+        public class Volkswagon : Car { };
+    }
+}
---
+++ CarDealer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using static CarDealer.Factory;
+
+namespace CarDealer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // MARK: Setup
+            Console.WriteLine("Hit ENTER to find the vehicles you're looking for!");
+            Console.ReadKey();
+
+            // MARK: Result
+            GetModels();
+            Console.ReadKey();
+        }
+
+        // MARK: Write your solution here...
+        public static void GetModels()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var types = assembly.GetTypes();
+
+            var abstractTypes = types.Where(t => t.IsSubclassOf(typeof(Car)) && !t.IsAbstract).ToList();
+
+            foreach (var type in abstractTypes)
+            {
+                Console.WriteLine(type.Name);
+            }
+
+        }
+    }
+}
